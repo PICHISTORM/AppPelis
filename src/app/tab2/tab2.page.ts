50947c024c2b4692e667c0be18d5db2855486302
PICHISTORM <davidalejandrot522@gmail.com>
2026-06-29 22:42:14 -0600

    issue 14

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonLabel, IonList, IonItem, IonListHeader, IonGrid, IonRow, IonSpinner, IonCol } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonSearchbar, IonLabel, IonList, IonItem, IonListHeader, IonGrid, IonRow, IonSpinner, IonCol]
 })
 export class Tab2Page {
+
+  textBuscar: string = '';
+  ideas: string[] = [
+    'Spider-Man',
+    'Batman',
+    'Iron Man',
+    'Superman',
+    'Wonder Woman',
+    'Doctor Strange',
+    'The Flash',
+    'Wolverine',
+    'Green Lantern',
+    'Thor',
+    'Nightwing',
+    'Daredevil',
+    'Aquaman',
+    'Deadpool',
+    'Martian Manhunter',
+    'Black Panther',
+    'Zatanna',
+    'Scarlet Witch',
+    'Green Arrow',
+    'Captain America'
+  ];
+
+  buscar($event: any) {
+    const valor= $event.detail.value;
+    console.log(valor);
+  }
 
   constructor() {}
 
